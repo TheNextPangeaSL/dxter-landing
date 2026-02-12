a12b208e3ebb58a87d75e5f26d06bce271aaef57
Antonio Paya <antonio.paya@thenextpangea.com>
2026-02-12 15:21:05 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://thenextpangeasl.github.io",
   base: "/dxter-landing",
+  trailingSlash: "always",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
